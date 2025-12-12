f5a3196bb0df33c7bc58b8a04d275d37a379998c
JonasABach <jobac@equinor.com>
2025-12-12 14:37:21 +0100

    a

--- server/tests/Todo.UnitTests/obj/Debug/net8.0/Todo.UnitTests.AssemblyInfo.cs
+++ server/tests/Todo.UnitTests/obj/Debug/net8.0/Todo.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a28a0735018589348067da11d28fdb0b77c4c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f033d6ec4e0cdfbec6f5a2a403f06cab68a60f13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
